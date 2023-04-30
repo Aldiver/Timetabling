4cdaf654db8d7b34b2a842e7b366abd23ea6f16e
aldiver <aldiver.alcoriza@gmail.com>
2023-04-30 23:37:25 +0800

    added title in login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,129 +1,119 @@
 <script setup>
-import { useForm, Head, Link } from '@inertiajs/inertia-vue3'
-import { mdiAccount, mdiAsterisk } from '@mdi/js'
-import LayoutGuest from '@/layouts/LayoutGuest.vue'
-import SectionFullScreen from '@/components/SectionFullScreen.vue'
-import CardBox from '@/components/CardBox.vue'
-import FormCheckRadioGroup from '@/components/FormCheckRadioGroup.vue'
-import FormField from '@/components/FormField.vue'
-import FormControl from '@/components/FormControl.vue'
-import BaseDivider from '@/components/BaseDivider.vue'
-import BaseButton from '@/components/BaseButton.vue'
-import BaseButtons from '@/components/BaseButtons.vue'
-import FormValidationErrors from '@/components/FormValidationErrors.vue'
-import NotificationBarInCard from '@/components/NotificationBarInCard.vue'
-import BaseLevel from '@/components/BaseLevel.vue'
+import { useForm, Head, Link } from "@inertiajs/inertia-vue3";
+import { mdiAccount, mdiAsterisk } from "@mdi/js";
+import LayoutGuest from "@/layouts/LayoutGuest.vue";
+import SectionFullScreen from "@/components/SectionFullScreen.vue";
+import CardBox from "@/components/CardBox.vue";
+import FormCheckRadioGroup from "@/components/FormCheckRadioGroup.vue";
+import FormField from "@/components/FormField.vue";
+import FormControl from "@/components/FormControl.vue";
+import BaseDivider from "@/components/BaseDivider.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import BaseButtons from "@/components/BaseButtons.vue";
+import FormValidationErrors from "@/components/FormValidationErrors.vue";
+import NotificationBarInCard from "@/components/NotificationBarInCard.vue";
+import BaseLevel from "@/components/BaseLevel.vue";
 
 const props = defineProps({
-  canResetPassword: Boolean,
-  status: {
-    type: String,
-    default: null
-  }
-})
+    canResetPassword: Boolean,
+    status: {
+        type: String,
+        default: null,
+    },
+});
 
 const form = useForm({
-  email: '',
-  password: '',
-  remember: []
-})
+    email: "",
+    password: "",
+    remember: [],
+});
 
 const submit = () => {
-  form
-    .transform(data => ({
-      ... data,
-      remember: form.remember && form.remember.length ? 'on' : ''
-    }))
-    .post(route('login'), {
-      onFinish: () => form.reset('password'),
-    })
-}
+    form.transform((data) => ({
+        ...data,
+        remember: form.remember && form.remember.length ? "on" : "",
+    })).post(route("login"), {
+        onFinish: () => form.reset("password"),
+    });
+};
 </script>
 
 <template>
-  <LayoutGuest>
-    <Head title="Login" />
+    <LayoutGuest>
+        <Head title="Login" />
 
-    <SectionFullScreen
-      v-slot="{ cardClass }"
-      bg="purplePink"
-    >
-      <CardBox
-        :class="cardClass"
-        is-form
-        @submit.prevent="submit"
-      >
-        <FormValidationErrors />
+        <SectionFullScreen v-slot="{ cardClass }" bg="purplePink">
+            <CardBox :class="cardClass" is-form @submit.prevent="submit">
+                <FormValidationErrors />
 
-        <NotificationBarInCard 
-          v-if="status"
-          color="info"
-        >
-          {{ status }}
-        </NotificationBarInCard>
+                <NotificationBarInCard v-if="status" color="info">
+                    {{ status }}
+                </NotificationBarInCard>
+                <h1 class="text-4xl md:text-5xl font-bold mb-6 text-left">
+                    DP - Timetabler
+                </h1>
+                <FormField
+                    label="Email"
+                    label-for="email"
+                    help="Please enter your email"
+                >
+                    <FormControl
+                        v-model="form.email"
+                        :icon="mdiAccount"
+                        id="email"
+                        autocomplete="email"
+                        type="email"
+                        required
+                    />
+                </FormField>
 
-        <FormField
-          label="Email"
-          label-for="email"
-          help="Please enter your email"
-        >
-          <FormControl
-            v-model="form.email"
-            :icon="mdiAccount"
-            id="email"
-            autocomplete="email"
-            type="email"
-            required
-          />
-        </FormField>
+                <FormField
+                    label="Password"
+                    label-for="password"
+                    help="Please enter your password"
+                >
+                    <FormControl
+                        v-model="form.password"
+                        :icon="mdiAsterisk"
+                        type="password"
+                        id="password"
+                        autocomplete="current-password"
+                        required
+                    />
+                </FormField>
 
-        <FormField
-          label="Password"
-          label-for="password"
-          help="Please enter your password"
-        >
-          <FormControl
-            v-model="form.password"
-            :icon="mdiAsterisk"
-            type="password"
-            id="password"
-            autocomplete="current-password"
-            required
-          />
-        </FormField>
+                <FormCheckRadioGroup
+                    v-model="form.remember"
+                    name="remember"
+                    :options="{ remember: 'Remember' }"
+                />
 
-        <FormCheckRadioGroup
-          v-model="form.remember"
-          name="remember"
-          :options="{ remember: 'Remember' }"
-        />
+                <BaseDivider />
 
-        <BaseDivider />
-
-        <BaseLevel>
-          <BaseButtons>
-            <BaseButton
-              type="submit"
-              color="info"
-              label="Login"
-              :class="{ 'opacity-25': form.processing }"
-              :disabled="form.processing"
-            />
-            <!-- <BaseButton
+                <BaseLevel>
+                    <BaseButtons>
+                        <BaseButton
+                            type="submit"
+                            color="info"
+                            label="Login"
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                        />
+                        <!-- <BaseButton
               v-if="canResetPassword"
               routeName="password.request"
               color="info"
               outline
               label="Remind"
             /> -->
-          </BaseButtons>
-          <!-- <Link
+                    </BaseButtons>
+                    <!-- <Link
             :href="route('register')"
           >
             Register
           </Link> -->
-        </BaseLevel>
-      </CardBox>
-    </SectionFullScreen>
-  </LayoutGuest>
+                </BaseLevel>
+            </CardBox>
+        </SectionFullScreen>
+    </LayoutGuest>
 </template>
